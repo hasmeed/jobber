2eeb7bc8a48a136ad2cdf24a4ac23b511dca2bc1
Hasmeed <hasmeedyusuf@yahoo.com>
2018-10-28 00:38:31 +0530

    added new fields and serializers

--- jobhunt/api/urls.py
+++ jobhunt/api/urls.py
@@ -3,7 +3,7 @@ from rest_framework_jwt.views import obtain_jwt_token
 from .views import parentCategory, Service
 
 from rest_framework_swagger.views import get_swagger_view
-schema_view = get_swagger_view('Friend Finder Api')
+schema_view = get_swagger_view('JobErrand Api developed by hasmeed')
 
 urlpatterns = [
     path('login/', obtain_jwt_token, name='api-login'),
--- jobhunt/api/views.py
+++ jobhunt/api/views.py
@@ -11,12 +11,18 @@ from rest_framework import permissions
 
 
 class parentCategory(generics.ListCreateAPIView):
+    """
+    Base API endpoint that shows all parent category and also accept post
+    """
     serializer_class = CategorySerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Category.objects.all()
 
 
 class Service(generics.ListCreateAPIView):
+    """
+    Base API endpoint that display all services and new service can also be posted to it
+    """
     serializer_class = ServiceSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Service.objects.all()
